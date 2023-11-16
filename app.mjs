28b0b1dfb5c9dd43c7a22c956135d41461ae30d2
brianzou03 <brianzou03@gmail.com>
2023-11-16 14:39:38 -0500

    Add error logging

--- app.mjs
+++ app.mjs
@@ -10,7 +10,6 @@ import rateLimit from 'express-rate-limit'; // Rate limiter
 const app = express();
 
 app.use(helmet()); // Helmet JS for security
-
 app.use(xss()); // XSS for security
 
 // rate limit IP
@@ -21,7 +20,7 @@ app.use(rateLimit({
 
 // Schemas
 const UserSchema = mongoose.model('users');
-const CartSchema = mongoose.model('carts'); 
+const CartSchema = mongoose.model('carts');
 const InventorySchema = mongoose.model('inventories');
 
 import url from 'url';
@@ -38,15 +37,20 @@ app.use(express.urlencoded({ extended: false }));
 // Use method-override middleware to handle the DELETE request
 app.use(methodOverride('_method'));
 
+// Adding a simple logging function
+function logData(message, data) {
+    console.log(message, JSON.stringify(data, null, 2));
+}
+
 // route handler for GET to home.hbs
 app.get('/', (req, res) => {
     UserSchema.find()
         .then((users) => {
-            console.log('Users found: ', users); // if users found
+            logData('Users found: ', users); // Log found users
             res.render('home', { users });
         })
         .catch((err) => {
-            console.error(err);
+            logData('Error finding users: ', err); // Log errors
             res.status(500).send('Internal server error');
         });
 });
@@ -55,11 +59,11 @@ app.get('/', (req, res) => {
 app.get('/cart_page', (req, res) => {
     CartSchema.find()
         .then((carts) => {
-            console.log('Cart found: ', carts); // if cart found
+            logData('Cart found: ', carts); // Log found carts
             res.render('cart_page', { carts });
         })
         .catch((err) => {
-            console.error(err);
+            logData('Error finding carts: ', err); // Log errors
             res.status(500).send('Internal server error');
         });
 });
@@ -68,11 +72,11 @@ app.get('/cart_page', (req, res) => {
 app.get('/inventory_page', (req, res) => {
     InventorySchema.find()
         .then((inventories) => {
-            console.log('Inventory found: ', inventories); // if inventory found
+            logData('Inventory found: ', inventories); // Log found inventories
             res.render('inventory_page', { inventories });
         })
         .catch((err) => {
-            console.error(err);
+            logData('Error finding inventories: ', err); // Log errors
             res.status(500).send('Internal server error');
         });
 });
@@ -80,8 +84,8 @@ app.get('/inventory_page', (req, res) => {
 // Route handler for adding user to home page
 app.post('/addUser', (req, res) => {
     const { username, bio } = req.body;
+    logData('Received data for addUser: ', req.body); // Log received data
 
-    // Input validation
     if (!username || !bio) {
         return res.status(400).send('Username and bio are required');
     }
@@ -106,23 +110,20 @@ app.post('/addUser', (req, res) => {
 app.post('/addToCart', (req, res) => {
     const { username, items } = req.body;
     let { itemPrices } = req.body;
+    logData('Received data for addToCart: ', req.body); // Log received data
 
-    // Convert items and itemPrices into arrays
     const itemsArray = items.split(',');
     itemPrices = itemPrices.split(',').map(price => price.trim());
 
-    // Check if the number of items matches the number of prices
     if (itemsArray.length !== itemPrices.length) {
         return res.render('error', { message: 'Error: The number of items and prices must match.' });
     }
 
-    // Validate that each itemPrice is a valid integer
     const invalidPrice = itemPrices.find(price => isNaN(price) || !Number.isInteger(Number(price)));
     if (invalidPrice !== undefined) {
         return res.render('error', { message: `Error: Invalid price value '${invalidPrice}' provided. Prices must be integers.` });
     }
 
-    // Convert itemPrices to integers
     itemPrices = itemPrices.map(price => parseInt(price, 10));
 
     const newCart = new CartSchema({
@@ -145,17 +146,15 @@ app.post('/addToCart', (req, res) => {
 // Route handler for adding item to inventory
 app.post('/addToInventory', (req, res) => {
     const { username, items, itemDescriptions } = req.body;
+    logData('Received data for addToInventory: ', req.body); // Log received data
 
-    // Input validation
     if (!username || !items || !itemDescriptions) {
         return res.status(400).send('Username, items, and descriptions are required');
     }
 
-    // Convert items and itemDescriptions into arrays
     const itemsArray = items.split(',');
     const descriptionsArray = itemDescriptions.split(',');
 
-    // Check if the number of items matches the number of descriptions
     if (itemsArray.length !== descriptionsArray.length) {
         return res.render('error', { message: 'Error: The number of items and descriptions must match.' });
     }
@@ -180,6 +179,7 @@ app.post('/addToInventory', (req, res) => {
 // Route handler for deleting user from home page
 app.delete('/deleteUser/:id', async (req, res) => {
     const userId = req.params.id;
+    logData('Received request to delete user with ID: ', userId); // Log user ID
 
     try {
         const deletedUser = await UserSchema.findByIdAndDelete(userId);
@@ -198,6 +198,7 @@ app.delete('/deleteUser/:id', async (req, res) => {
 // Route handler for deleting item from cart
 app.delete('/deleteCartItem/:id', async (req, res) => {
     const cartItemId = req.params.id;
+    logData('Received request to delete cart item with ID: ', cartItemId); // Log cart item ID
 
     try {
         const deletedItem = await CartSchema.findByIdAndDelete(cartItemId);
@@ -216,6 +217,7 @@ app.delete('/deleteCartItem/:id', async (req, res) => {
 // Route handler for deleting item from inventory
 app.delete('/deleteInventoryItem/:id', async (req, res) => {
     const inventoryItemId = req.params.id;
+    logData('Received request to delete inventory item with ID: ', inventoryItemId); // Log inventory item ID
 
     try {
         const deletedItem = await InventorySchema.findByIdAndDelete(inventoryItemId);
